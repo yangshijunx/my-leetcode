e9c055aa8abde7ea5d143f16df3b403ce2378bed
yangshijun <yangshijun777@gmail.com>
2023-09-18 19:58:39 +0800

    feat: init

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig, loadEnv } from 'vite'
+import "./main.ts"
+
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+  // console.log("环境变量", env)
+  return {
+    // vite 配置
+    // define: {
+    //   __APP_ENV__: JSON.stringify(env.APP_ENV),
+    // },
+  }
+})
